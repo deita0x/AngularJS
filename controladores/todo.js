15b2933f8fa95a3d20f1a4fcd7d611cd980b08b6
deita0x <josdeita@gmail.com>
2014-08-08 19:14:31 -0500

    Added methods to improve the todo list

--- controladores/todo.js
+++ controladores/todo.js
@@ -1,4 +1,41 @@
-function TodoController ($scope) {
+function TodoController($scope) {
   $scope.todos = [{text: 'Ser super heroico con AngularJS', done: true},
                  {text: 'Aprender AngularJS', done: false}];
+
+  /**
+   * Agrega una nueva tarea si el texto del input no esta vacio
+   */
+  $scope.addTodo = function() {
+    if ($scope.todoText) {
+      $scope.todos.push({text: $scope.todoText, done: false});
+      $scope.todoText = '';
+    }
+  };
+
+  /**
+   * Retorna las tareas que aun no se han marcado
+   * @return {number}
+   */
+  $scope.remaining = function() {
+    var count = 0;
+
+    angular.forEach($scope.todos, function(todo) {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
+
+  /**
+   * Elimina las tareas que ya estan completadas
+   */
+  $scope.remove = function() {
+    var oldTodos = $scope.todos;
+    $scope.todos = [];
+
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) {
+        $scope.todos.push(todo);
+      }
+    });
+  };
 }
