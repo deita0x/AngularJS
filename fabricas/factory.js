a6822f9e61011b5f0a2539dd9bb5db5c290eb028
deita0x <josdeita@gmail.com>
2014-08-19 00:20:56 -0500

    added share methods between controllers

--- fabricas/factory.js
+++ fabricas/factory.js
@@ -1,13 +1,36 @@
 var myApp = angular.module('myApp', []);
 
-myApp.factory('Data', function() {
-  return {message: 'Saludos desde la fabrica!'}
+myApp.factory('Fabrica', function() {
+  var service = {
+    objeto: {message: 'Saludos desde la fabrica!'},
+    msjNuevo: function(msj) {
+      service.objeto.message = msj;
+    },
+    msjInicial: function() {
+      service.objeto['message'] = 'Saludos desde la fabrica!';
+    }
+  };
+  return service;
 });
 
-myApp.controller('ControllerOne', function($scope, Data) {
-  $scope.data = Data;
+myApp.controller('ControllerOne', function($scope, Fabrica) {
+  $scope.nuevo = function() {
+    Fabrica.msjNuevo($scope.newMessage);
+  };
+
+  $scope.data = Fabrica.objeto;
 });
 
-myApp.controller('ControllerTwo', function($scope, Data) {
-  $scope.data = Data;
+myApp.controller('ControllerTwo', function($scope, Fabrica) {
+  $scope.nuevo = function() {
+    Fabrica.msjNuevo($scope.newMessage);
+  };
+
+  $scope.data = Fabrica.objeto;
+});
+
+myApp.controller('ControllerThree', function($scope, Fabrica) {
+  $scope.reset = function() {
+    Fabrica.msjInicial();
+  };
 });
